3d9f49eaf265b36ff679068c2cbcbd255bc288d7
icypt <icypt_xy@126.com>
2018-07-09 17:22:22 +0800

    提交................

---
+++ java-learn-project/src/main/java/com/icypt/io/example/outputstream/DataOutputStream01.java
@@ -0,0 +1,18 @@
+package com.icypt.io.example.outputstream;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+public class DataOutputStream01{
+   public static void main(String[] args) throws IOException{
+       File file = new File("d:" + File.separator +"hello.txt");
+       char[] ch = { 'A', 'B', 'C' };
+       DataOutputStream out = null;
+       out = new DataOutputStream(new FileOutputStream(file));
+       for(char temp : ch){
+           out.writeChar(temp);
+       }
+       out.close();
+    }
+}
---
+++ java-learn-project/src/main/java/com/icypt/io/example/piped/TestPiped.java
@@ -0,0 +1,24 @@
+package com.icypt.io.example.piped;
+
+import java.io.IOException;
+
+/**
+ * 测试类
+ * */
+public class TestPiped {
+    public static void main(String[] args) {
+        Send send = new Send();
+        Receive receive = new Receive();
+        try {
+            //管道连接
+            send.getPipedOut().connect(receive.getReceive());
+            new Thread(send).start();
+            new Thread(receive).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(send).start();
+        new Thread(receive).start();
+    }
+}
