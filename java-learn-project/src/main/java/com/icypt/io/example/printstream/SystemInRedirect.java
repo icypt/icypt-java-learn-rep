8eb61258613f24536ea63c3da596768d6f213ae2
icypt <icypt_xy@126.com>
2018-07-10 14:40:49 +0800

    提交................

---
+++ java-learn-project/src/main/java/com/icypt/io/example/printstream/SystemInRedirect.java
@@ -0,0 +1,17 @@
+package com.icypt.io.example.printstream;
+
+import java.io.*;
+
+/**
+ *System.in重定向
+ * */
+public class SystemInRedirect {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:\\input1.txt");
+        System.setIn(new FileInputStream(file));
+
+        byte[] bytes = new byte[1024];
+        int len = System.in.read(bytes);
+        System.out.println(new String(bytes,0, len));
+    }
+}
