0b171431dbba79f2c8f535ad6053586bc225ab70
icypt <icypt_xy@126.com>
2018-08-02 10:55:16 +0800

    引入springboot功能

---
+++ java-learn-project/src/main/java/com/icypt/JavaLearnProjectApplication.java
@@ -0,0 +1,11 @@
+package com.icypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearnProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearnProjectApplication.class);
+    }
+}
